608931a9504245d9523f7f02eae3b30d9fa22e54
fonehackerx <fonehackerx@gmail.com>
2022-01-08 14:41:58 +0530

    Create personalized.js

---
+++ assets/js/personalized.js
@@ -0,0 +1,12 @@
+if (marx > 6) {
+    document.getElementById("awesome").style = "display:inline-block";
+}
+if (marx >= 5 && marx <= 6) {
+    document.getElementById("secondtier").style = "display:inline-block";
+}
+if (marx >= 3 && marx <= 4) {
+    document.getElementById("thikthak").style = "display:inline-block";
+}
+if (marx >= 0 && marx <= 2) {
+    document.getElementById("mahabekar").style = "display:inline-block";
+}
